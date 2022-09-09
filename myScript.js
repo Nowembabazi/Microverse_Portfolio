626ae68eda9e4139188bf69bf9a12647f1dbbc65
nickson <nowembabazin8@gmail.com>
2022-09-09 17:26:45 -0500

    Add functionality to my portfolio

--- myScript.js
+++ myScript.js
@@ -13,7 +13,160 @@ closeMobileItems.forEach((element) =>
   element.addEventListener("click", closeMobileMenu)
 );
 /* Project Detail objects */
+const projectDetail = [{
+  title: ['Tonic'],
+  exprience: ['CANOPY', 'Back End Dev', '2015'],
+  desription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  popDescription:'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  technologies: ['html', 'css', 'javascript'],
+  popupTech: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
+  imageDesk: 'images/IMG-20220826-WA0008.jpg',
+  imageDeskPopup: 'images/IMG-20220826-WA0008.jpg',
+},
+{
+  title: 'Multi-Post Stories',
+  exprience: ['FACEBOOK', 'Back End Dev', '2015'],
+  desription: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+  popDescription:'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+  technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+  popupTech: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
+  imageDesk: 'images/snapshot4.svg',
+  imageDeskPopup: 'images/snapshot4.svg',
+},
+{
+  title: 'Facebook 360',
+  exprience: ['FACEBOOK', 'Back End Dev', '2015'],
+  desription: 'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+  popDescription:'Exploring the future of media in Facebook first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
+  technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+  popupTech: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
+  imageDesk: 'images/Snapshootportfolio.svg',
+  imageDeskPopup: 'images/Snapshootportfolio.svg',
+},
+{
+  title: 'Uber Navigation',
+  exprience: ['UBER', 'Lead Developer', '2018'],
+  desription: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+  desription: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+  popDescription: '',
+  technologies: ['html', 'Ruby on rails', 'css', 'javascript'],
+  popupTech: ['html', 'css', 'javaScript', 'github', 'Ruby', 'Bootstrap'],
+  imageDesk:'images/SnapshootPortfolios.svg',
+  imageDeskPopup: 'images/SnapshootPortfolios.svg',
+},
+];
+/* End of project detail object */
 
+/* Populate object to html elements when the page loads */
+for (let i = 0; i < projectDetail.length; i += 1) {
+  let tech = '';
+  let experienceList = '';
+
+  for (let j = 0; j < projectDetail[i].technologies.length; j += 1) {
+    tech += `<li class="technology"><a href="#">${projectDetail[i].technologies[j]}</a></li>`;
+  }
+
+  for (let k = 0; k < projectDetail[i].exprience.length; k += 1) {
+    experienceList += (k === 0) ? `<li><a href="#" class="canopy">${projectDetail[i].exprience[k]}</a></li>` : `<li><a href="#">${projectDetail[i].exprience[k]}</a></li>`;
+    if (k !== projectDetail[i].exprience.length - 1) experienceList += '<li><a href="#"><img src="" alt=""></a></li>';
+  }
+  document.querySelector('#portfolio').innerHTML += `<div class="main-container">
+  <div class="grid-item" id="work-1">
+    <div class="desk-item1"><img src="${projectDetail[i].imageDesk}" alt=""></div>
+     <div class="desk-col">
+       <div class="flex-item2">
+         <h2>${projectDetail[i].title}</h2>
+       </div>
+       <div class="flex-item2-desk">
+         <h2>${projectDetail[i].title}</h2>
+       </div>
+       <div class="flex-item3">
+         <ul class="inner-element">
+           ${experienceList}
+         </ul>
+       </div>
+       <div class="flex-item4">
+         <p>${projectDetail[i].desription}</p>
+       </div>
+       <div class="flex-item5">
+       <ul class="inner-flex-item">
+        ${tech}
+         </ul>
+       </div>
+       <div class="flex-item6"><button data-index="${i}" class="see-project">See project</button></div>
+    </div>
+   </div>`;
+}
+/* End of project detail population when the pages loads */
+
+/* Display Popup modal */
+
+const popupModal = document.querySelector('#portfolioPopup');
+const displayPopupModal = (i) => {
+  let tech = '';
+  let techPop = '';
+  let experienceList = '';
+
+  for (let j = 0; j < projectDetail[i].technologies.length; j += 1) {
+    tech += `<li class="technology"><a href="#">${projectDetail[i].technologies[j]}</a></li>`;
+  }
+
+  for (let j = 0; j < projectDetail[i].popupTech.length; j += 1) {
+    techPop += `<li class="lang-group" id="mobile">${projectDetail[i].popupTech[j]}</li>`;
+  }
+
+  for (let k = 0; k < projectDetail[i].exprience.length; k += 1) {
+    experienceList += (k === 0) ? `<li class="list-main">${projectDetail[i].exprience[k]}</li>` : `<li class="titles-item">${projectDetail[i].exprience[k]}</li>`;
+  }
+  document.querySelector('#portfolioPopup').innerHTML = `
+  <div class="works-flex">
+      <div>
+        <div class="works-flex-pop">
+          <h3 class="intro-text-works">${projectDetail[i].title}</h3>
+          <span> <a onclick="closePopupModal()" href="" class="close-popup">X</a></span>
+      </div>
+      <ul class="works-flex-item titles-container">
+      ${experienceList}
+      </ul>
+      </div>
+      <div class="works-flex-item work-image">
+          <img src="${projectDetail[i].imageMobilePopup}" class="flex-item1" alt="${projectDetail[i].title} project screenshot"/>
+          <img src="${projectDetail[i].imageDeskPopup}" class="desk-item1" alt="${projectDetail[i].title} project screenshot"/>
+      </div>
+      <div class="project-tags">
+          <div class="work-description">
+            <p class="works-flex-item works-description">
+              ${projectDetail[i].popDescription}
+            </p>
+          </div>
+          <div class="tags">
+              <ul class="lang-container mobile">
+              ${tech}
+              </ul>
+              <ul class="lang-container">
+              ${techPop}
+              </ul>
+              <div class="btn-tags">
+                <a href="${projectDetail[i].seeLive}" class="btn-see-project">See Live
+                  <i class="fa fa-external-link" aria-hidden="true"></i>
+                </a>
+                <a href="${projectDetail[i].seeSource}" class="btn-see-project">See Source
+                  <i class="fa fa-github-square" aria-hidden="true"></i>
+                </a>
+            </div>
+          </div>
+      </div>
+      </div>
+  `;
+  popupModal.style.display = 'block';
+};
+document.querySelectorAll('.see-project').forEach((row) => row.addEventListener('click', () => displayPopupModal(row.getAttribute('data-index'))));
+
+const closeModal = document.querySelector('#portfolioPopup');
+const closePopupModal = () => {
+  closeModal.style.display = 'none';
+};
+/* End of display popup modal */
 /* Contact Form validation */
 const regex = "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,}$";
 document.querySelector("#form").addEventListener("submit", (e) => {
