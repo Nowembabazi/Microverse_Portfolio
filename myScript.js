64dad3c4ec123c349f1268ec0c71525c77ab3f0b
nickson <nowembabazin8@gmail.com>
2022-09-10 12:50:28 -0500

    find bug

--- myScript.js
+++ myScript.js
@@ -7,7 +7,7 @@ const closeMobileMenu = () => {
   menuItem.style.display = "none";
 };
 document
-  .querySelector(".openMenu")
+  
   .addEventListener("click", displayMobileMenu);
 closeMobileItems.forEach((element) =>
   element.addEventListener("click", closeMobileMenu)
